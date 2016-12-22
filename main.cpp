a0b3555b0a990267317f2dc563898715f1061c4e
Matthew Slane <matthew.slane1@gmail.com>
2016-12-22 17:03:13 -0600

    added input (still has much debug)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,79 @@
 #include "line.h"
 #include "grid.h"
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-  Grid puzzle(5, 5);
-  vector<int> row0, row2, row3, row4, col0, col1, col2, col3, col4;
-  row0.push_back(1);
-  row2.push_back(1);
-  row3.push_back(1);
-  row3.push_back(2);
-  row4.push_back(4);
-  col0.push_back(1);
-  col1.push_back(1);
-  col1.push_back(3);
-  col2.push_back(1);
-  col3.push_back(2);
-  col4.push_back(1);
+  int m, n, i, clue;
+  unsigned int j;
+  string line;
+  stringstream tokens;
+  vector<int> line_clues;
+  cin >> m >> n;
 
-  puzzle.add_row_clues(0, row0);
-  puzzle.add_row_clues(2, row2);
-  puzzle.add_row_clues(3, row3);
-  puzzle.add_row_clues(4, row4);
+  Grid puzzle(m, n);
 
-  puzzle.add_col_clues(0, col0);
-  puzzle.add_col_clues(1, col1);
-  puzzle.add_col_clues(2, col2);
-  puzzle.add_col_clues(3, col3);
-  puzzle.add_col_clues(4, col4);
+  //flush line
+  getline(cin, line);
 
-  bool valid = puzzle.solve();
-  cout << valid << endl;
+  for(i = 0; i < m; i++)
+  {
+    getline(cin, line);
+    tokens.clear();
+    tokens.str(line);
+    line_clues.clear();
 
+    cout << "doing row #" << i << endl;
+    cout << tokens.str() << endl;
+
+    while(tokens >> clue)
+    {
+      cout << "f ";
+      line_clues.push_back(clue);
+    }
+    cout << endl;
+
+    if(!(line_clues.size() == 0 && line_clues[0] == 0))
+    {
+      puzzle.add_row_clues(i, line_clues);
+    }
+
+    for(j = 0; j < line_clues.size(); j++)
+    {
+      cout << line_clues[j] << " ";
+    }
+    cout << endl;
+  }
+
+  for(i = 0; i < n; i++)
+  {
+    getline(cin, line);
+    tokens.clear();
+    tokens.str(line);
+    line_clues.clear();
+
+    cout << "doing col #" << i << endl;
+
+    while(tokens >> clue)
+    {
+      line_clues.push_back(clue);
+    }
+
+    if(!(line_clues.size() == 1 && line_clues[0] == 0))
+    {
+      puzzle.add_col_clues(i, line_clues);
+    }
+
+    for(j = 0; j < line_clues.size(); j++)
+    {
+      cout << line_clues[j] << " ";
+    }
+    cout << endl;
+  }
+
+  bool solved = puzzle.solve();
   puzzle.print_grid();
-  return 0;
 }
